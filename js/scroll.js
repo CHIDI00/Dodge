2caf02d268e4fa6e2740002f52019bd43c58b5f5
CHIDI MATHAIAS <chidimathayas@gmail.com>
2023-07-08 18:28:58 +0100

    Sixteenth Commit 🚀

--- js/scroll.js
+++ js/scroll.js
@@ -14,27 +14,3 @@ navLinks.addEventListener('click', function(e) {
   }
 })
 
-
-
-
-
-
-const handleClick = function (e, opacity) {
-  if (e.target.classList.contains('nav-link')) {
-    const link = e.target;
-    const siblings = link.closest('.nav').querySelectorAll('.nav-ink');
-    const logo = link.closest('.nav').querySelector('img');
-
-    siblings.forEach(el => {
-      if (el !== link) el.classList.add('active_nav');
-      else anchor.classList.remove('active_nav');
-
-    });
-    // .classList.remove('active_nav')
-  }
-}
-
-// Passing "argument" into handler
-navLinks.addEventListener('click', handleClick)
-
-// nav.addEventListener('mouseout', handleHover.bind(1))
